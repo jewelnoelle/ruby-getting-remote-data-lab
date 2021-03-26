38adf998e3709bb74e46e1fb9838ae56e0066059
Jewel Carrier-Davis <jujicb@gmail.com>
2021-03-26 08:14:35 -0400

    commit tree 40cad9e52595d3341b2a31167677f25dc5c9e959

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,32 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+class GetRequester
+
+    @@persons_arr = []
+    @@persons_hash = {}
+    # @@final_arr = []
+
+    def initialize(string)
+        @URL = string
+    end
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        people = JSON.parse(self.get_response_body)
+        
+        @@persons = people.each_with_index do |person, index|
+            @@persons_arr.clear
+            @@persons_arr << person[index]
+        end
+        @@persons
+    end
+    
+end
